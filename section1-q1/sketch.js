acdb598058e78b3bf16e77f8d898fd4ef58a6cab
AyanoGoto <91936397+AyanoGoto@users.noreply.github.com>
2021-10-19 11:49:40 +0900

    Update2 1-q1

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -12,8 +12,20 @@ function setup(){
   arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
+
   const red = color(196, 0, 0);
   const blue = color(14, 47, 146);
   const green = color(22, 131, 46);
   const gray = color(77);
+
+  fill(blue);
+  noStroke();
+  ellipse(300,100,100,100);
+  fill(255);
+  ellipse(295,95,87,87);
+  fill(green);
+  ellipse(290,95,82,80);
+  fill(255);
+  ellipse(288,100,75,75)
+//文字情報をここに追加（したい）
 }
