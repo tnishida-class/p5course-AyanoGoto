28e3a4f7f6dda7476611bd0baed1deb99d0f784c
AyanoGoto <91936397+AyanoGoto@users.noreply.github.com>
2021-10-19 12:04:43 +0900

    Update 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,20 @@
 // 小手調べ
 function setup(){
+  noFill();
+  //ellipse(50,50,10);//i=0
+  //ellipse(50,50,20);//i=1
+  //ellipse(50,50,30);//i=2
+  //ellipse(50,50,40);//i=3
+
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    let r=i*10+10;//10ずつ増えている，最初１０だから+10
+    if(i<5){//blue
+      stroke(0,0,255);
+    }
+    else{//red
+      stroke(255,0,0);
+    }
+    ellipse(50,50,r);//先に色，後にellapse。図形描く前に色
   }
 }
