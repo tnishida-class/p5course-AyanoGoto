e87826b98b9f610c186eb2b5591da9d732170236
AyanoGoto <91936397+AyanoGoto@users.noreply.github.com>
2021-10-20 16:34:57 +0900

    Update 2-q2 ver2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -14,62 +14,14 @@ function setup() {
       }
       rect(size*i, size*j, size, size);//rectより先に条件
 
-      if(j<3){
-       fill(196, 0, 0);
-       noStroke();
-       ellipse(38,12,20,20);//１列２行
-       noStroke();
-       ellipse(88,12,20,20);//１列４行
-       noStroke();
-       ellipse(138,12,20,20);//１列６行
-       noStroke();
-       ellipse(188,12,20,20);//１列８行
-       noStroke();
-       ellipse(12,38,20,20);//２列１行
-       noStroke();
-       ellipse(62,38,20,20);//２列３行
-       noStroke();
-       ellipse(112,38,20,20);//２列５行
-       noStroke();
-       ellipse(162,38,20,20);//２列７行
-       noStroke();
-       ellipse(38,62,20,20);//3列２行
-       noStroke();
-       ellipse(88,62,20,20);//３列４行
-       noStroke();
-       ellipse(138,62,20,20);//３列６行
-       noStroke();
-       ellipse(188,62,20,20);//３列８行
-       //初め三列は赤丸にしたい
-       //ellipse省略して書きたいなーー
+      if((i+j)%2==1&&j<3){
+       fill(196, 0, 0);//初め三列は赤丸にしたい,ellipse省略して書きたいなーー
      　}
-     else if(j>4 && j<8){
+     else if((i+j)%2==1&&j>4){
        fill(0,0,0);//６〜８列目は黒丸
-       noStroke();
-       ellipse(12,138,20,20)
-       noStroke();
-       ellipse(62,138,20,20)
-       noStroke();
-       ellipse(112,138,20,20)
-       noStroke();
-       ellipse(162,138,20,20)//６列目ここまで
-       noStroke();
-       ellipse(38,162,20,20)
-       noStroke();
-       ellipse(88,162,20,20)
-       noStroke();
-       ellipse(138,162,20,20)
-       noStroke();
-       ellipse(188,162,20,20)//７列目ここまで
-       noStroke();
-       ellipse(12,188,20,20)
-       noStroke();
-       ellipse(62,188,20,20)
-       noStroke();
-       ellipse(112,188,20,20)
-       noStroke();
-       ellipse(162,188,20,20)//８列目ここまで
      　　}
+     noStroke();
+     ellipse(size*i+ 25/2, size*j+ 25/2, 20)
     }
   }
 }
