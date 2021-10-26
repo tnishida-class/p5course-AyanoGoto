521713c2c45e6be3b99caf37781427e4593514a1
AyanoGoto <91936397+AyanoGoto@users.noreply.github.com>
2021-10-26 11:35:30 +0900

    Update 2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -16,7 +16,11 @@ function setup() {
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
+　drawArcs(cream, black, maxR * 0.75);
+  drawArcs(green, red, maxR * 0.5);
+  drawArcs(cream, black, maxR * 0.45);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
+  drawCircle(green, maxR * 0.10);
   drawCircle(red, maxR * 0.05);
 }
 
