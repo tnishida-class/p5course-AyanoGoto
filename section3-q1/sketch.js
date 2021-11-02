51a8edf8fa1652dfb0f89dc6f32893947e3b230e
AyanoGoto <91936397+AyanoGoto@users.noreply.github.com>
2021-11-02 11:13:57 +0900

    Update 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -10,7 +10,23 @@ function setup(){
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  if(keyIsDown(" ".charCodeAt(0))){
+    count = (count + 5 ) % cycle;  
+  }//周期的に変化するようにする
+  //if(count==100)　count=0と，count=(count+1)%cycleは同じ
+  else{
+    count = (count + 1 ) % cycle;
+  }
+  //サイクルで割ったあまりが周期的に変化
+  //ドローを何回したかを数えているだけ，サイクルで割ったあまりまでを
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+
+  if(count < 50){
+    ellipse(width/2, height/2,count);
+  }
+  else{
+    ellipse(width/2, height/2, 100-count);
+  }
 }
+//周期は一緒でcountの進み具合を帰る
+//押されていたら２増えて，押されてない時は１増えるなど,動きは変えないでcountのとこだけ
